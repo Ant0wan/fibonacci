8e25ad783d347781c2542daf2f4786e31c56e4bd
Antoine Barthelemy <antoine@linux.com>
2024-10-14 13:34:22 +0200

    init

--- lib/fibonacci.go
+++ lib/fibonacci.go
@@ -9,16 +9,6 @@ type Matrix struct {
 	a, b, c, d *big.Int
 }
 
-//// Function to multiply two 2x2 matrices of big.Int
-//func (m *Matrix) Multiply(n *Matrix) *Matrix {
-//	return &Matrix{
-//		a: new(big.Int).Add(new(big.Int).Mul(m.a, n.a), new(big.Int).Mul(m.b, n.c)),
-//		b: new(big.Int).Add(new(big.Int).Mul(m.a, n.b), new(big.Int).Mul(m.b, n.d)),
-//		c: new(big.Int).Add(new(big.Int).Mul(m.c, n.a), new(big.Int).Mul(m.d, n.c)),
-//		d: new(big.Int).Add(new(big.Int).Mul(m.c, n.b), new(big.Int).Mul(m.d, n.d)),
-//	}
-//}
-// Function to multiply two 2x2 matrices of big.Int (in-place)
 func (m *Matrix) Multiply(n *Matrix) *Matrix {
 	// Temporary variables to store the result to avoid allocating new big.Int objects
 	a := new(big.Int).Add(new(big.Int).Mul(m.a, n.a), new(big.Int).Mul(m.b, n.c))
@@ -35,7 +25,33 @@ func (m *Matrix) Multiply(n *Matrix) *Matrix {
 	return m
 }
 
-// Function to compute the matrix raised to the power n using binary exponentiation
+//// Function to compute the matrix raised to the power n using binary exponentiation
+//func (m *Matrix) Power(n *big.Int) *Matrix {
+//	// Identity matrix
+//	result := &Matrix{
+//		a: big.NewInt(1),
+//		b: big.NewInt(0),
+//		c: big.NewInt(0),
+//		d: big.NewInt(1),
+//	}
+//	base := m
+//
+//	// Temporary variables
+//	zero := big.NewInt(0)
+//	one := big.NewInt(1)
+//	exponent := new(big.Int).Set(n)
+//
+//	// Binary exponentiation
+//	for exponent.Cmp(zero) > 0 {
+//		if new(big.Int).And(exponent, one).Cmp(one) == 0 {
+//			result = result.Multiply(base)
+//		}
+//		base = base.Multiply(base)
+//		exponent.Rsh(exponent, 1)
+//	}
+//
+//	return result
+//}
 func (m *Matrix) Power(n *big.Int) *Matrix {
 	// Identity matrix
 	result := &Matrix{
@@ -44,24 +60,29 @@ func (m *Matrix) Power(n *big.Int) *Matrix {
 		c: big.NewInt(0),
 		d: big.NewInt(1),
 	}
-	base := m
+	base := &Matrix{
+		a: new(big.Int).Set(m.a),
+		b: new(big.Int).Set(m.b),
+		c: new(big.Int).Set(m.c),
+		d: new(big.Int).Set(m.d),
+	}
 
 	// Temporary variables
 	zero := big.NewInt(0)
 	one := big.NewInt(1)
-	exponent := new(big.Int).Set(n)
 
-	// Binary exponentiation
-	for exponent.Cmp(zero) > 0 {
-		if new(big.Int).And(exponent, one).Cmp(one) == 0 {
+	// Binary exponentiation: iteratively square the matrix
+	for n.Cmp(zero) > 0 {
+		if new(big.Int).And(n, one).Cmp(one) == 0 {
 			result = result.Multiply(base)
 		}
 		base = base.Multiply(base)
-		exponent.Rsh(exponent, 1)
+		n.Rsh(n, 1)
 	}
 
 	return result
 }
+
 
 // Function to compute the nth Fibonacci number using matrix exponentiation
 func FibonacciMatrix(n *big.Int) *big.Int {
--- test.sh
+++ test.sh
@@ -1,9 +1,20 @@
 # Basic cases
-curl 'http://localhost:8000/fib?n=222323230983049820347032634926918623872197391273109876543456789876543212345678998776862117639817639186876329187632917863918'
+curl 'http://localhost:8000/fib?n=1'
+curl 'http://localhost:8000/fib?n=21'
+curl 'http://localhost:8000/fib?n=321'
+curl 'http://localhost:8000/fib?n=4321'
+curl 'http://localhost:8000/fib?n=54321'
+curl 'http://localhost:8000/fib?n=654321'
+curl 'http://localhost:8000/fib?n=7654321'
+curl 'http://localhost:8000/fib?n=87654321'
+curl 'http://localhost:8000/fib?n=987654321'
+curl 'http://localhost:8000/fib?n=1987654321'
+
+#curl 'http://localhost:8000/fib?n=222323230983049820347032634926918623872197391273109876543456789876543212345678998776862117639817639186876329187632917863918'
 
 # Malformed
-curl 'http://localhost:8000/fib?n=22232fib?n=99999'
-curl 'http://localhost:8000/fib?n=%3Cscript%3Ealert(1)%3C/script%3E'
+#curl 'http://localhost:8000/fib?n=22232fib?n=99999'
+#curl 'http://localhost:8000/fib?n=%3Cscript%3Ealert(1)%3C/script%3E'
 
 # Big numbers
 
