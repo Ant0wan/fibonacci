933b6e6a25ced7fcec162498260cd4ab70841712
Antoine Barthelemy <antoine@linux.com>
2024-10-14 22:24:32 +0200

    init

--- benchmark.sh
+++ benchmark.sh
@@ -1,4 +1,4 @@
-# Basic cases
+#!/bin/bash
 export TIME="%E"
 > $1 1>/dev/null
 /usr/bin/time curl -s 'http://localhost:8000/fib?n=1' 2>>$1 1>/dev/null
@@ -10,14 +10,3 @@ export TIME="%E"
 /usr/bin/time curl -s 'http://localhost:8000/fib?n=7654321' 2>>$1 1>/dev/null
 /usr/bin/time curl -s 'http://localhost:8000/fib?n=27654321' 2>>$1 1>/dev/null
 /usr/bin/time curl -s 'http://localhost:8000/fib?n=112233445' 2>>$1 1>/dev/null
-
-# Malformed
-#curl 'http://localhost:8000/fib?n=22232fib?n=99999'
-#curl 'http://localhost:8000/fib?n=%3Cscript%3Ealert(1)%3C/script%3E'
-
-# Big numbers
-
-# Spamming
-#while true; do curl 'http://localhost:8000/fib?n=notanumber'; done
-#while true; do curl 'http://localhost:8000/fib?n=98765435678982320932873927392739797397397873292012730197301973019730197301987320918732910872301723017820731209731029719070'; done
-
